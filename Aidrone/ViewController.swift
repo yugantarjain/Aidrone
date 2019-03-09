eecd5251c774bd91c579882b9c42d80de688f8b9
Yugantar Jain <yugantar8jain@gmail.com>
2019-03-10 03:22:06 +0530

    tbc

--- Aidrone/ViewController.swift
+++ Aidrone/ViewController.swift
@@ -14,15 +14,21 @@ class ViewController: UIViewController {
     
     let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
     let ref = Database.database().reference()
+    let ann = MKPointAnnotation.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //        Map.region = MKCoordinateRegion(center: xy, span: span)
-        let ann = MKPointAnnotation.init()
-        ann.coordinate.latitude = 10
-        ann.coordinate.longitude = 77
-        Map.addAnnotation(ann)
+        ref.child("People").observe(.value) { (snapshot) in
+            for data in snapshot.children.allObjects as! [DataSnapshot]
+            {
+                let a = data.value as? [String: AnyObject]
+                let lati = a?["Lat"] as! Double
+                let longi = a?["Long"] as! Double
+                self.updateCentre(lat: lati, long: longi)
+            }
+        }
     }
     @IBOutlet weak var Map: MKMapView!
     
@@ -30,6 +36,10 @@ class ViewController: UIViewController {
     {
         let xy = CLLocationCoordinate2D(latitude: lat, longitude: long)
         Map.region = MKCoordinateRegion(center: xy, span: span)
+        
+        ann.coordinate.latitude = lat
+        ann.coordinate.longitude = long
+        Map.addAnnotation(ann)
     }
 }
 
