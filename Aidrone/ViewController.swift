4009d670166981fd78537923eabb0555c8513e20
Yugantar Jain <yugantar8jain@gmail.com>
2019-03-10 02:04:30 +0530

    Map outlet added.

--- Aidrone/ViewController.swift
+++ Aidrone/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var Map: MKMapView!
+    
 
 }
 
